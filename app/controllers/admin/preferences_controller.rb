94b573cd6c7682d3d1c92b7dc06369931c4c4b3d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-19 14:06:58 -0500

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -2,12 +2,5 @@ class Admin::PreferencesController < ApplicationController
     def index
     end
 
-    def allow_create_artists
-        @preference = Preference.all.first
-
-        redirect_to artists_path
-    end
-
-    def allow_create_songs
-    end
+    
 end
